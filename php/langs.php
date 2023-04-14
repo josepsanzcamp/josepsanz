7e27816f44ea8d2a9d2334cd2acd3c93bbbf1426
Josep Sanz <josep.sanz@saltos.org>
2023-04-14 13:22:35 +0200

    Canviar l'ordre dels enllaços del projecte OpenROCS

--- php/langs.php
+++ php/langs.php
@@ -65,9 +65,9 @@ $labels = array(
                 "title" => "OpenROCS v2.0",
                 "description" => "OpenROCS conforms an independent control layer for robotic observatories. It is a master scheduler of the processes that the system has to execute and a monitor of the overall state of the observatory. It controls the end-to-end data flow and the housekeeping processes by handling a set of predefined events. The former are responsible for ensuring that the system executes the routine operation by maximizing the scientific return of the facility. While the latter consider all those anomalous situations that could put the observatory at risk and activate corrective or mitigation actions. All the applications running at the observatory and the hardware components interact with it. It is released under GNU/GPL public license.",
                 "links" => array(
-                    "upcommons.upc.edu" => "https://upcommons.upc.edu/handle/2099.1/26215",
                     "github.com" => "https://github.com/josepsanzcamp/OpenROCS",
                     "sourceforge.net" => "http://sourceforge.net/projects/openrocs",
+                    "upcommons.upc.edu" => "https://upcommons.upc.edu/handle/2099.1/26215",
                 )
             ),
             "gralla" => array(
@@ -103,7 +103,7 @@ $labels = array(
                     "josepsanzcamp.github.io" => "https://josepsanzcamp.github.io/covid19",
                     "github.com" => "https://github.com/josepsanzcamp/covid19",
                 )
-            ),
+            )
         )
     ),
     "en" => array(
@@ -167,9 +167,9 @@ $labels = array(
                 "title" => "OpenROCS v2.0",
                 "description" => "OpenROCS conforms an independent control layer for robotic observatories. It is a master scheduler of the processes that the system has to execute and a monitor of the overall state of the observatory. It controls the end-to-end data flow and the housekeeping processes by handling a set of predefined events. The former are responsible for ensuring that the system executes the routine operation by maximizing the scientific return of the facility. While the latter consider all those anomalous situations that could put the observatory at risk and activate corrective or mitigation actions. All the applications running at the observatory and the hardware components interact with it. It is released under GNU/GPL public license.",
                 "links" => array(
-                    "upcommons.upc.edu" => "https://upcommons.upc.edu/handle/2099.1/26215",
                     "github.com" => "https://github.com/josepsanzcamp/OpenROCS",
                     "sourceforge.net" => "http://sourceforge.net/projects/openrocs",
+                    "upcommons.upc.edu" => "https://upcommons.upc.edu/handle/2099.1/26215",
                 )
             ),
             "gralla" => array(
@@ -205,7 +205,7 @@ $labels = array(
                     "josepsanzcamp.github.io" => "https://josepsanzcamp.github.io/covid19",
                     "github.com" => "https://github.com/josepsanzcamp/covid19",
                 )
-            ),
+            )
         )
     ),
     "ca" => array(
@@ -269,9 +269,9 @@ $labels = array(
                 "title" => "OpenROCS v2.0",
                 "description" => "OpenROCS conforms an independent control layer for robotic observatories. It is a master scheduler of the processes that the system has to execute and a monitor of the overall state of the observatory. It controls the end-to-end data flow and the housekeeping processes by handling a set of predefined events. The former are responsible for ensuring that the system executes the routine operation by maximizing the scientific return of the facility. While the latter consider all those anomalous situations that could put the observatory at risk and activate corrective or mitigation actions. All the applications running at the observatory and the hardware components interact with it. It is released under GNU/GPL public license.",
                 "links" => array(
-                    "upcommons.upc.edu" => "https://upcommons.upc.edu/handle/2099.1/26215",
                     "github.com" => "https://github.com/josepsanzcamp/OpenROCS",
                     "sourceforge.net" => "http://sourceforge.net/projects/openrocs",
+                    "upcommons.upc.edu" => "https://upcommons.upc.edu/handle/2099.1/26215",
                 )
             ),
             "gralla" => array(
@@ -307,7 +307,7 @@ $labels = array(
                     "josepsanzcamp.github.io" => "https://josepsanzcamp.github.io/covid19",
                     "github.com" => "https://github.com/josepsanzcamp/covid19",
                 )
-            ),
+            )
         )
     )
 );
