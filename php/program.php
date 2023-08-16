af1d12f4e68d28165013ad25c1c8de16ba508de5
Josep Sanz <josep.sanz@saltos.org>
2023-08-16 09:04:58 +0200

    Afegir hash per forçar control de cache

--- php/program.php
+++ php/program.php
@@ -8,8 +8,8 @@ $js = js_minify($js);
 file_put_contents("js/all.min.js", $js);
 $css = css_minify($css);
 file_put_contents("css/all.min.css", $css);
-$html = js_minify2($html, "js/all.min.js");
-$html = css_minify2($html, "css/all.min.css");
+$html = js_minify2($html, "js/all.min.js?" . md5_file("js/all.min.js"));
+$html = css_minify2($html, "css/all.min.css?" . md5_file("css/all.min.css"));
 $html = html_minify($html);
 file_put_contents("index.php", $html);
 foreach (array("ca","es","en") as $lang) {
