1c6034be3a8d8480fc2d650488000a601057c582
Josep Sanz <josep.sanz@saltos.org>
2025-02-05 07:41:28 +0100

    Some profile updates

--- pdf/makepdf.sh
+++ pdf/makepdf.sh
@@ -9,7 +9,7 @@
 for i in catalan spanish english; do
     gs -sDEVICE=pdfwrite -dPDFSETTINGS=/printer -dNOPAUSE -dBATCH -dSAFER -sOutputFile=cv_josep_sanz_${i}.pdf.2 cv_josep_sanz_${i}.pdf
     gs -sDEVICE=pdfwrite -dPDFSETTINGS=/ebook -dNOPAUSE -dBATCH -dSAFER -sOutputFile=cv_josep_sanz_${i}_full.pdf cv_josep_sanz_${i}.pdf.2
-    gs -sDEVICE=pdfwrite -dPDFSETTINGS=/ebook -dNOPAUSE -dBATCH -dSAFER -dFirstPage=5 -dLastPage=9 -sOutputFile=cv_josep_sanz_${i}_preview.pdf cv_josep_sanz_${i}.pdf.2
+    gs -sDEVICE=pdfwrite -dPDFSETTINGS=/ebook -dNOPAUSE -dBATCH -dSAFER -dFirstPage=5 -dLastPage=8 -sOutputFile=cv_josep_sanz_${i}_preview.pdf cv_josep_sanz_${i}.pdf.2
     rm cv_josep_sanz_${i}.pdf
     rm cv_josep_sanz_${i}.pdf.2
     pdftocairo -png -transp cv_josep_sanz_${i}_preview.pdf cv_josep_sanz_${i}_preview
